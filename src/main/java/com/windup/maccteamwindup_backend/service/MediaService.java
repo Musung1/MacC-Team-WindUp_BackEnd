8b39a9ec13afe47a1546d0243a07ba42a21f3089
musung <musungkim1352@gmail.com>
2023-10-23 12:42:42 +0900

    [Feat] MediaService 구현 #7

---
+++ src/main/java/com/windup/maccteamwindup_backend/service/MediaService.java
@@ -0,0 +1,30 @@
+package com.windup.maccteamwindup_backend.service;
+
+import org.apache.tomcat.util.http.fileupload.impl.IOFileUploadException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class MediaService {
+    @Value("${audio-dir}")
+    private String uploadDir;
+
+    public void fileUpload(MultipartFile multipartFile) {
+        Path copyOfLocation = Paths.get(uploadDir + File.separator + StringUtils.cleanPath(multipartFile.getOriginalFilename()));
+        try {
+            Files.copy(multipartFile.getInputStream(), copyOfLocation, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
